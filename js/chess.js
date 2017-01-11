78dff3d1582f9f41f3cb63277b5c61acd7fbc8b7
ErgsAndTerst <coling1231999@hotmail.com>
2017-01-11 16:00:37 -0500

    Changed header, more JS stuff

--- js/chess.js
+++ js/chess.js
@@ -1,27 +1,54 @@
-var file = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-var rank = ['1', '2', '3', '4', '5', '6', '7', '8'];
-var xVal = [0, 1, 2, 3, 4, 5, 6, 7];
-var yVal = [0, 1, 2, 3, 4, 5, 6, 7];
-var board = document.getElementByID('board');
+// Declare arrays for chess board locations
+var horizontal  = [0, 1, 2, 3, 4, 5, 6, 7];
+var file        = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+var vertical    = [0, 1, 2, 3, 4, 5, 6, 7];
+var rank        = ['1', '2', '3', '4', '5', '6', '7', '8'];
 
-function returnSquare(x, y) {
-    var square =
-    {
-        file: file[x-1],
-        rank: rank[y-1],
-        x: xVal[x-1],
-        y: yVal[y-1]
-    };
-
-    return square;
-}
+var board       = document.getElementById('board');
 
 function returnBoard() {
-    return 0;
+    // Declare empty object
+    var boardInfo = {};
+    for (i = 0; i < 8; i++) {
+        for (j = 0; j < 8; j++) {
+            // Loop through all combinations of (0-7, 0-7)
+            boardInfo[returnSquare(horizontal[i], vertical[j]).id] = returnSquare(i, j);
+            // Append data from returnSquare to the boardInfo object
+        }
+    }
+    return boardInfo;
 }
 
-function initSquares() {
+function returnSquare(x, y) {
+    // Creates an object of info on each square of the board
+    var squareInfo =
+    {
+        file:     file[x],
+        fileNum:  horizontal[x],
+        id:       file[x] + rank[y],
+        occupied: isOccupied(x, y),
+        piece:    pieceType(x, y),
+        rank:     rank[y],
+        rankNum:  vertical[y],
+    };
+    return squareInfo;
+}
 
-    return 0
+function isOccupied(x, y) {
+    var square = document.getElementById(file[x] + rank[y]);
+    if (square.children[0].className == 'occupied') {
+        return true;
+    } else {
+        return false;
+    };
+}
+
+function pieceType(x, y) {
+    var square = document.getElementById(file[x] + rank[y]);
+    if (isOccupied(x, y) == false) {
+        return 'none';
+    } else {
+        return square.children[0].id;
+    }
 }
 
