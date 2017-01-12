acf33029203891d08c7ebb272385cf2a7d6a81c5
GREYBCOL000 <GREYBCOL000@HASD.local>
2017-01-12 12:37:37 -0500

    Added JSON info, chess opening page

--- js/chess.js
+++ js/chess.js
@@ -1,10 +1,8 @@
 // Declare arrays for chess board locations
 var horizontal  = [0, 1, 2, 3, 4, 5, 6, 7];
+var vertical    = horizontal;
 var file        = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
-var vertical    = [0, 1, 2, 3, 4, 5, 6, 7];
 var rank        = ['1', '2', '3', '4', '5', '6', '7', '8'];
-
-var board       = document.getElementById('board');
 
 function returnBoard() {
     // Declare empty object
@@ -17,6 +15,12 @@ function returnBoard() {
         }
     }
     return boardInfo;
+}
+
+function initBoard(opening) {
+    for (i = 0; i < 64; i++) {
+        // Set IDs of chess pieces according to JSON information
+    }
 }
 
 function returnSquare(x, y) {
